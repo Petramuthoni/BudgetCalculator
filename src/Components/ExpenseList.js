021f055c1557e64e3415234fbebd190ff0f29ffe
PETRA <petra>
2021-08-25 21:55:57 +0300

    adding project

---
+++ src/Components/ExpenseList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem'
+// import React from "react";
+
+import { MdDelete } from "react-icons/md";
+const ExpenseList = ({ expenses, handleDelete, handleEdit, clearItems }) => {
+  return (
+    <>
+      <ul className="list">
+        {expenses.map(expense => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              expense={expense}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
+          );
+        })}
+      </ul>
+      {expenses.length > 0 && (
+        <button className="btn" onClick={clearItems}>
+          clear expenses
+          <MdDelete className="btn-icon" />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ExpenseList;
